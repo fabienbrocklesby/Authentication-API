519c4eec7791d8e514298e0a13b9a0a17abe3105
Fabien Brocklesby <fabienbrocklesby@icloud.com>
2022-05-21 11:34:03 +1200

    Password Reset

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import { createUserHandler, forgotPasswordHandler, verifyUserHandler } from '../controller/user.controller';
+import { createUserHandler, forgotPasswordHandler, resetPasswordHandler, verifyUserHandler } from '../controller/user.controller';
 import validateResource from '../middleware/validateResource';
-import { createUserSchema, forgotPasswordSchema, verifyUserSchema } from '../schema/user.schema';
+import { createUserSchema, forgotPasswordSchema, resetPasswordSchema, verifyUserSchema } from '../schema/user.schema';
 
 const router = express.Router();
 
@@ -20,5 +20,11 @@ router.post(
     '/api/users/forgotpassword',
     validateResource(forgotPasswordSchema),
     forgotPasswordHandler);
+
+router.post(
+    '/api/users/resetpassword/:id/:passwordResetCode',
+    validateResource(resetPasswordSchema),
+    resetPasswordHandler,
+);
 
 export default router;
--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -38,6 +38,25 @@ export const forgotPasswordSchema = object({
     }),
 });
 
+export const resetPasswordSchema = object({
+    params: object({
+        id: string(),
+        passwordResetCode: string(),
+    }),
+    body: object({
+        password: string({
+            required_error: 'Password is required',
+        }).min(6, 'Password is too short - should be min 6 chars'),
+        passwordConfirmation: string({
+            required_error: 'Password confirmation is required',
+        }),
+    }).refine((data) => data.password === data.passwordConfirmation, {
+        message: 'Password do not match',
+        path: ['passwordConfirmation'],
+    }),
+});
+
 export type CreateUserInput = TypeOf<typeof createUserSchema>['body'];
 export type VerifyUserInput = TypeOf<typeof verifyUserSchema>['params'];
 export type ForgotPasswordInput = TypeOf<typeof forgotPasswordSchema>['body'];
+export type ResetPasswordInput = TypeOf<typeof resetPasswordSchema>
